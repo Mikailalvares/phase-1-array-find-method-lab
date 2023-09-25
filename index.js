fb0ebbf283a5263329e69bf5ed69132c0585cf8d
Mikailalvares <mikailismail260@gmail.com>
2023-09-25 05:12:08 -0900

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// code your solution here
+const { expect } = require('chai'); 
+
+function superbowlWin(record) {
+  const winRecord = record.find(entry => entry.result === "W");
+  return winRecord ? winRecord.year : undefined;
+}
+
+describe('superbowlWin', function () {
+  it('returns the year the Denver Broncos won the superbowl', function () {
+    const record = [
+      { year: '2015', result: 'W' },
+      { year: '2016', result: 'L' },
+    
+    ];
+    const result = superbowlWin(record);
+    expect(result).to.equal('2015');
+  });
+
+  it('returns undefined if the record has no win objects', function () {
+    const sadReality = [
+      { year: '2016', result: 'L' },
+      { year: '2017', result: 'L' },
+      { year: '2018', result: 'L' }
+    ];
+    const result = superbowlWin(sadReality);
+    expect(result).to.be.undefined; 
+  });
+});
